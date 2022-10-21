3c90eba4df4f177637a8c5db81bb42be334e9ee9
junwon <todayhappymod365@gmail.com>
2022-10-21 17:32:01 +0900

    쿼리스트링+html에 값 넘겨주는 방식

--- src/main/java/Hello/Hellospring/Controller/HelloController.java
+++ src/main/java/Hello/Hellospring/Controller/HelloController.java
@@ -1,13 +1,22 @@
 package Hello.Hellospring.Controller;
 
-
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "Spring!!");
         return "hello";
     }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-temp";
+    }
+    //  쿼리스트링 받는 방식 + 값을 넘겨주는 방식
+
 }
